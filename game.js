923469557205f1a9f285143380c0a2f3a5197d95
Jason Anders <realjasona@gmail.com>
2019-05-23 08:56:55 -0600

    add 5 rounds of play

--- game.js
+++ game.js
@@ -1,4 +1,7 @@
 let playerGuess;
+let playerWins = 0;
+let computerWins = 0;
+let gameCount = 0;
 
 function computerPlay() {
     return Math.floor(Math.random() * 3);
@@ -20,18 +23,35 @@ function playRound(playerSelection, computerSelection) {
     }
 
     if (computerSelection == playerSelection) {
-        return("Tied it up");
+        console.log("Tied it up");
     } else if (computerSelection == "rock" && playerSelection == "paper") {
-        return("You win");
+        console.log("You win");
+        playerWins++
     } else if (computerSelection == "scissors" && playerSelection == "rock") {
-        return("You win");
+        console.log("You win");
+        playerWins++
     } else if (computerSelection == "paper" && playerSelection == "scissors") {
-        return("You Win");
+        console.log("You Win");
+        playerWins++
     } else {
-        return("Sorry, the computer won");
+        console.log("Sorry, the computer won");
+        computerWins++
     }
 }
 
-const playerSelection = 'paper'
-const computerSelection = computerPlay()
-console.log(playRound(playerSelection, computerSelection))
+function game() {
+    playerSelection = prompt("Please make your pick (Paper, Rock, or Scissors)").toLowerCase();
+    computerSelection = computerPlay()
+    playRound(playerSelection, computerSelection);
+    gameCount++
+}
+
+while (gameCount < 5) {
+    game();
+}
+
+console.log(`Player won: ${playerWins} \n Computer won: ${computerWins}`);
+
+// const playerSelection = 'paper'
+// const computerSelection = computerPlay()
+// console.log(playRound(playerSelection, computerSelection))
